1680ed0ba49cbf74ad6af8ca1e18a57c71fc077f
Unknown <pbegkas98@outlook.com>
2019-05-28 17:15:39 +0300

    Removed node, added move checking algorithm

--- minimax.java
+++ minimax.java
@@ -6,25 +6,65 @@
 
 public class minimax{
 	
-	private class Node{
+	
+	
+	
+	public int[] choose(int[] cubes, int player) { // cubes is an list with black in position 0 and white in 1
 		
-		Node[] children = new Node[4];
-		Node parent;
-		int depth;
-		int value;
-		int[] cubes;
+		int[] value = new int [4];
+		int move;
 		
-		// constructor
-		Node(Node p, int[] c, int d) {
+		// checking which players turn is
+		
+		// checking which moves can be done
+		
+		// remove 2 black and 1 white
+		if( (cubes[0] > 1) && (cubes[1] > 0) ) {
 			
-			parent = p;
-			cubes = c;
-			depth = d;
+			int[] c0 = new int[2]; 
+			c0[0] = cubes[0] - 2;
+			c0[1] = cubes[1] - 1;
+			
+			choose(c0, )
+		}
+		
+		// remove 2 white and 1 black
+		if( (cubes[0] > 0) && (cubes[1] > 1) ) {
+			
+			int[] c1 = new int[2];
+			c1[0] = cubes[0] - 1;
+			c1[1] = cubes[1] - 2;
 			
 		}
 		
+		// remove 1 black
+		if(cubes[0] > 0) {
+			
+			int[] c2 = new int[2];
+			c2[0] = cubes[0] - 1;
+			c2[1] = cubes[1];
+			
+		}
+		
+		// remove 1 white
+		if(cubes[1] > 0) {
+			
+			int[] c3 = new int[2];
+			c3[0] = cubes[0];
+			c3[1] = cubes[1] - 1;
+			
+			
+			
+		}
+		
+		int [] output = new int[2];
 		
 		
+		
+		output[0] = value;
+		output[1] = move;
+		return output;
 	}
+
 	
 }
